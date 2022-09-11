f4c311d7a9a9fb1d2272e2f7ad8bdb7b8554af7f
Nero <1600959@uab.cat>
2022-09-11 18:15:09 +0200

    Poolrooms, swim enemy. Still some game engine bugs that slow down the level building process

--- Scenes/Death.gd
+++ Scenes/Death.gd
@@ -11,11 +11,6 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Replay_button_down():
 	get_tree().change_scene("res://Scenes/Video.tscn")
 
